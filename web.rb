fe98a93301bcc221c6844a03217dcd18f73af025
louisray97 <louisray97@gmail.com>
2017-12-01 15:23:07 +0000

    Update web.rb

--- web.rb
+++ web.rb
@@ -1,38 +1,3 @@
-
-require 'sinatra'
-require 'stripe'
-require 'dotenv'
-require 'json'
-require 'encrypted_cookie'
-# Set your secret key: remember to change this to your live secret key in production
-# See your keys here: https://dashboard.stripe.com/account/apikeys
-Stripe.api_key = "sk_test_fg6lxRP5xB7Hk8BnlBatro95"
-
-# Token is created using Checkout or Elements!
-# Get the payment token ID submitted by the form:
-token = params[:stripeToken]
-
-# Create a Customer:
-customer = Stripe::Customer.create(
-  :email => "paying.user@example.com",
-  :source => token,
-)
-
-# Charge the Customer instead of the card:
-charge = Stripe::Charge.create(
-  :amount => 1000,
-  :currency => "gbp",
-  :customer => customer.id,
-)
-
-# YOUR CODE: Save the customer ID and other info in a database for later.
-
-# YOUR CODE (LATER): When it's time to charge the customer again, retrieve the customer ID.
-charge = Stripe::Charge.create(
-  :amount => 1500, # $15.00 this time
-  :currency => "gbp",
-  :customer => customer_id, # Previously stored, then retrieved
-)
 require 'sinatra'
 require 'stripe'
 require 'dotenv'
